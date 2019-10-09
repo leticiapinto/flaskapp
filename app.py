5ceec6edc188d04d9443a042f0330c6d508cacdc
EC2 Default User <ec2-user@ip-172-31-7-101.us-east-2.compute.internal>
2019-10-09 00:06:41 +0000

    working with forms

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def home_func():
-    return render_template("home.html")
+@app.route('/')
+def student():
+   return render_template('student.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template('result.html',result = result)
+
+if __name__ == '__main__':
+    app.run()

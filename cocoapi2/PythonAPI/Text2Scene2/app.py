94532e663b7d50e03f95ee28c9dda062a6f6974b
EC2 Default User <ec2-user@ip-172-31-28-101.us-east-2.compute.internal>
2019-10-11 21:29:40 +0000

    uploading files october 11

--- cocoapi2/PythonAPI/Text2Scene2/app.py
+++ cocoapi2/PythonAPI/Text2Scene2/app.py
@@ -39,9 +39,12 @@ def result():
  
       if(int(option) == 3):
         #print(3)
-        msg_to_script = result['sentence1']
-        result2 = {'Option': 'Layout', 'Input': result['sentence1']}
-          
+        msg_to_script2 = result['sentence1']
+        result2 = {'Option': 'Layout', 'Input': result['sentence1']}  
+        run_system = './tools/composites_demo.py --for_visualization=True --use_super_category=True --use_patch_background=True --n_shape_hidden=256 --where_attn=2 --where_attn_2d=True --pretrained=composites_final'
+        os.system( run_system +' "' + msg_to_script2 +'" "' + postfix +'"')
+        hists = os.listdir('static/layout_'+postfix+'/layout_samples/')
+        hists = ['layout_'+postfix+'/layout_samples/' + file for file in hists]
       
       return render_template('result.html',result = result2, text_to_send = text_to_send, hists = hists)
 if __name__ == '__main__':
